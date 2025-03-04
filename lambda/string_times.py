11e9ef34d43d2a0683cf3bcd47b21a568dd4c88a
junhongliao77@gmail.com <junhongliao77@gmail.com>
2025-03-03 23:26:29 -0500

    configured lambda library

---
+++ lambda/string_times.py
@@ -0,0 +1,9 @@
+string_times = lambda s, n: s * n
+# string_times = lambda s, n: ''.join(s for i in range(n))
+
+# def double(x):
+#     return x * 2
+
+# nums = [1, 2, 3, 4]
+# doubled = list(map(double, nums))
+# print(doubled)  # Output: [2, 4, 6, 8]
