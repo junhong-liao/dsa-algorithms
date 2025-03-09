061e48c490f77ad303ede8eea771d919f74e3b4a
junhongliao77@gmail.com <junhongliao77@gmail.com>
2025-03-08 19:14:41 -0500

    container_with_most_water.py updated

--- two_pointers/container_with_most_water.py
+++ two_pointers/container_with_most_water.py
@@ -1,22 +1,31 @@
+'''
+approach:
+    two pointers, left and right
+    move the smaller pointer, as it is the bottleneck
+    this is because area is calculated as base * height, where height = min(left, right)
+
+'''
+
 import math
 
+# optimal approach
 class Solution:
-    def maxArea(self, height: list[int]) -> int:
-        start, end = 0, len(height) - 1
-        max_area = -math.inf
-        while start < end:
-            # INCORRECT: current_area = (end - start + 1) * min(height[start], height[end])
-            # incorrect because the width holds water inside, not counting the walls as volume
-            # correct approach is end - start
-            current_area = (end - start) * min(height[start], height[end])
-            max_area = max(current_area, max_area)
-            if height[start] > height[end]: 
-                end -= 1
-            elif height[start] < height[end]:
-                start += 1
-            else:
-                if height[start + 1] > height[end - 1]:
-                    start += 1
-                else:
-                    end -= 1
+    def maxArea(self, height: List[int]) -> int:
+        left, right, max_area = 0, len(height) - 1, 0
+        while left < right:
+            base = right - left
+            area = base * min(height[left], height[right])
+            max_area = max(max_area, area)
+            if height[left] <= height[right]: left += 1
+            else: right -= 1
+        return max_area
+
+# minimized 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right, max_area = 0, len(height) - 1, 0
+        while left < right:
+            max_area = max((right-left)*min(height[left],height[right]),max_area)
+            if height[left] <= height[right]: left += 1
+            else: right -= 1
         return max_area
