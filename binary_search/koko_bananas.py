686ab3c825f18db6f673bffbed6e4a7b10acfe22
junhongliao77@gmail.com <junhongliao77@gmail.com>
2025-03-11 21:57:31 -0400

    kokos_bananas.py added to binary_search

--- binary_search/koko_bananas.py
+++ binary_search/koko_bananas.py
@@ -1,56 +1,25 @@
-class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int: 
-        max_pile = 0
-        for p in piles:
-            max_pile = max(p, max_pile)
-
-        if len(piles) == 1:
-            return -(-p//h)
-
-        candidates = [x for x in range(1, max_pile + 1)] # cant divide by zero. also, recall range stops 1 before
-
-        # binary search:
-        low, high = 0, len(candidates) - 1
-        while low <= high:
-            mid = low + (high - low) // 2
-            c = candidates[mid]
-            c_hours = self.check_candidate_helper(c, piles)
-            if c_hours == h:
-                return c
-            if c_hours > h: # need to eat faster
-                low = mid + 1
-            if c_hours < h: # can eat slower
-                high = mid - 1
-
-    # maybe should have written it all out via example instead of only logic
-    def check_candidate_helper(self, k: int, piles: List[int]) -> int:
-        sum = 0
-        for p in piles:
-            sum += -(-p//k) # ceil equivalent, // makes negatives bigger
-        return sum
-    
-# s = Solution()
-# piles = [3,6,7,11]
-# h = 8
-# # piles = [30,11,23,4,20]
-# # h = 6
-# print(s.minEatingSpeed(piles, h))
-
-# above is my best shot. below is neetcode's sln.
+import math
+from typing import List
 
 class Solution:
-    def minEatingSpeed(self, piles: list[int], h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
-        while l <= r:
-            k = (l + r) // 2
+	def minEatingSpeed(self, piles: List[int], h: int) -> int:
+		low, high = 1, max(piles) # REMEMBER TO START AT 1, avoid divison by zero
+		res = math.inf
+		while low <= high:
+			k = low + (high - low) // 2
+			hours = sum([math.ceil(pile / k) for pile in piles])
 
-            totalTime = 0
-            for p in piles:
-                totalTime += math.ceil(float(p) / k)
-            if totalTime <= h:
-                res = k
-                r = k - 1
-            else:
-                l = k + 1
-        return res
+            # if total time (hours) exceeds h, need to increase eating rate, because eating rate increase = total time decrease
+			# here, we went a little too low
+			if hours > h: low = k + 1
+
+            # if total time is under or equal to h, we can try decreasing the eating rate, because eating rate decrease = total time decrease.
+			else:
+				res = min(res, k)
+				high = k - 1
+		return res
+	
+piles=[3,6,7,11]
+h=8
+s = Solution()
+print(s.minEatingSpeed(piles, h))
